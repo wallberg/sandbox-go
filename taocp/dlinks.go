d9cb2f16a2a088d41763f758bff7760722765a0b
Ben Wallberg <wallberg@umd.edu>
2020-11-17 12:36:08 -0500

    update dlinks.ExactCover() to goto flow
    Convert from pseudo goto flow using an event loop
    to real goto flow

--- taocp/dlinks.go
+++ taocp/dlinks.go
@@ -38,20 +38,19 @@ func ExactCover(items []string, options [][]string, secondary []string,
 	stats *Stats, visit func(solution [][]string)) {
 
 	var (
-		n1       int      // number of primary items
-		n2       int      // number of secondary items
-		n        int      // total number of items
-		name     []string // name of the item
-		llink    []int    // right link of the item
-		rlink    []int    // left link of the item
-		top      []int
-		llen     []int
-		ulink    []int
-		dlink    []int
-		level    int
-		state    []int // search state
-		debug    bool  // is debug enabled?
-		progress bool  // are progress reports enabled?
+		n1    int      // number of primary items
+		n2    int      // number of secondary items
+		n     int      // total number of items
+		name  []string // name of the item
+		llink []int    // right link of the item
+		rlink []int    // left link of the item
+		top   []int
+		llen  []int
+		ulink []int
+		dlink []int
+		level int
+		state []int // search state
+		debug bool  // is debug enabled?
 	)
 
 	dump := func() {
@@ -81,7 +80,6 @@ func ExactCover(items []string, options [][]string, secondary []string,
 			stats.Nodes = 0
 			stats.Solutions = 0
 			debug = stats.Debug
-			progress = stats.Progress
 		}
 
 		// Fill out the item tables
@@ -335,129 +333,120 @@ func ExactCover(items []string, options [][]string, secondary []string,
 	// X1 [Initialize.]
 	initialize()
 
-	var i int
+	var (
+		i int
+		j int
+		p int
+	)
 
-	step := 2
-
-	for {
-		nextStep := step + 1 // Default value of nextStep
-
-		switch step {
-
-		case 2:
-			// X2. [Enter level l.]
-
-			if debug {
-				fmt.Printf("X2. level=%d, x=%v\n", level, state[0:level])
-			}
-
-			if stats != nil {
-				stats.Levels[level]++
-				stats.Nodes++
-			}
-
-			if rlink[0] == 0 {
-				// visit the solution
-				if stats != nil {
-					stats.Solutions++
-				}
-				if debug {
-					fmt.Println("X2. Visit the solution")
-				}
-				lvisit()
-				nextStep = 8
-			}
-			if progress {
-				if level > stats.MaxLevel {
-					stats.MaxLevel = level
-				}
-				if stats.Nodes >= stats.Theta {
-					showProgress()
-					stats.Theta += stats.Delta
-				}
-			}
-
-		case 3:
-			// X3. [Choose i.]
-			i = mrv()
-
-			if debug {
-				fmt.Printf("X3. Choose i=%d (%s)\n", i, name[i])
-			}
-
-		case 4:
-			// X4. [Cover i.]
-			if debug {
-				fmt.Printf("X4. Cover i=%d (%s)\n", i, name[i])
-			}
-			cover(i)
-			state[level] = dlink[i]
-
-		case 5:
-			// X5. [Try x_l.]
-			if debug {
-				fmt.Printf("X5. Try l=%d, x[l]=%d\n", level, state[level])
-			}
-			if state[level] == i {
-				nextStep = 7
-			} else {
-				p := state[level] + 1
-				for p != state[level] {
-					j := top[p]
-					if j <= 0 {
-						p = ulink[p]
-					} else {
-						cover(j)
-						p++
-					}
-				}
-				level++
-				nextStep = 2
-			}
-
-		case 6:
-			// X6. [Try again.]
-			if debug {
-				fmt.Println("X6. Try again")
-			}
-
-			if stats != nil {
-				stats.Nodes++
-			}
-
-			p := state[level] - 1
-			for p != state[level] {
-				j := top[p]
-				if j <= 0 {
-					p = dlink[p]
-				} else {
-					uncover(j)
-					p--
-				}
-			}
-			i = top[state[level]]
-			state[level] = dlink[state[level]]
-			nextStep = 5
-
-		case 7:
-			// X7. [Backtrack.]
-			if debug {
-				fmt.Println("X7. Backtrack")
-			}
-			uncover(i)
-
-		case 8:
-			// [Leave level l.]
-			if debug {
-				fmt.Printf("X8. Leaving level %d\n", level)
-			}
-			if level == 0 {
-				return
-			}
-			level--
-			nextStep = 6
-		}
-
-		step = nextStep
+X2:
+	// X2. [Enter level l.]
+	if debug {
+		fmt.Printf("X2. level=%d, x=%v\n", level, state[0:level])
 	}
+
+	if stats != nil {
+		stats.Levels[level]++
+		stats.Nodes++
+
+		if stats.Progress {
+			if level > stats.MaxLevel {
+				stats.MaxLevel = level
+			}
+			if stats.Nodes >= stats.Theta {
+				showProgress()
+				stats.Theta += stats.Delta
+			}
+		}
+	}
+
+	if rlink[0] == 0 {
+		// visit the solution
+		if debug {
+			fmt.Println("X2. Visit the solution")
+		}
+		if stats != nil {
+			stats.Solutions++
+		}
+		lvisit()
+		goto X8
+	}
+
+	// X3. [Choose i.]
+	i = mrv()
+
+	if debug {
+		fmt.Printf("X3. Choose i=%d (%s)\n", i, name[i])
+	}
+
+	// X4. [Cover i.]
+	if debug {
+		fmt.Printf("X4. Cover i=%d (%s)\n", i, name[i])
+	}
+	cover(i)
+	state[level] = dlink[i]
+
+X5:
+	// X5. [Try x_l.]
+	if debug {
+		fmt.Printf("X5. Try l=%d, x[l]=%d\n", level, state[level])
+	}
+	if state[level] == i {
+		goto X7
+	}
+	p = state[level] + 1
+	for p != state[level] {
+		j := top[p]
+		if j <= 0 {
+			p = ulink[p]
+		} else {
+			cover(j)
+			p++
+		}
+	}
+	level++
+	goto X2
+
+X6:
+	// X6. [Try again.]
+	if debug {
+		fmt.Println("X6. Try again")
+	}
+
+	if stats != nil {
+		stats.Nodes++
+	}
+
+	p = state[level] - 1
+	for p != state[level] {
+		j = top[p]
+		if j <= 0 {
+			p = dlink[p]
+		} else {
+			uncover(j)
+			p--
+		}
+	}
+	i = top[state[level]]
+	state[level] = dlink[state[level]]
+	goto X5
+
+X7:
+	// X7. [Backtrack.]
+	if debug {
+		fmt.Println("X7. Backtrack")
+	}
+	uncover(i)
+
+X8:
+	// X8. [Leave level l.]
+	if debug {
+		fmt.Printf("X8. Leaving level %d\n", level)
+	}
+	if level == 0 {
+		return
+	}
+	level--
+	goto X6
+
 }
