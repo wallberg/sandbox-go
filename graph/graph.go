31d6c731c9dcd40030b46236474f729f0c15f890
Ben Wallberg <ben.wallberg@gmail.com>
2021-01-23 16:07:18 -0500

    Add graph.ConnectedSubsetsVertex

--- graph/graph.go
+++ graph/graph.go
@@ -1,6 +1,11 @@
 package graph
 
 import (
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+
 	"github.com/yourbasic/graph"
 )
 
@@ -68,4 +73,217 @@ func CartesianProduct(g graph.Iterator, h graph.Iterator) *graph.Mutable {
 	}
 
 	return gh
+}
+
+// Arc represents an arc variable for use with ARCS, NAME, and TIP,
+// as in TAOCP Section 7.
+type Arc struct {
+	v    int   // an edge desination vertex
+	cost int64 // edge cost
+	next *Arc  // next Arc node in the list
+}
+
+// String describes an Arc as a string
+func (a *Arc) String() string {
+	return fmt.Sprintf("(arc=%d:%d)", a.v, a.cost)
+}
+
+// Arcs generates a singly linked list of Arc nodes, with one node for each
+// edge that emanates from vertex v in graph g.  Arcs are sorted in
+// vertex order
+func Arcs(g graph.Iterator, v int) (arcs *Arc) {
+	costs := make(map[int]int64) // Map distination vertices to costs
+	var edges []int
+
+	g.Visit(v, func(w int, c int64) bool {
+		edges = append(edges, w)
+		costs[w] = c
+		return false
+	})
+
+	sort.Ints(edges)
+
+	a := arcs
+	for _, w := range edges {
+		if a == nil {
+			arcs = &Arc{v: w, cost: costs[w]}
+			a = arcs
+		} else {
+			a.next = &Arc{v: w, cost: costs[w]}
+			a = a.next
+		}
+	}
+
+	return arcs
+}
+
+var debug bool
+
+func init() {
+	debug = false
+}
+
+// ConnectedSubsetsVertex generates all connected subsets in g of size n which
+// contain vertex v. Implements TAOCP Algorithm R from 7.2.2 Exercise 75.
+func ConnectedSubsetsVertex(g graph.Iterator, n int, v int,
+	visit func([]int) (halt bool)) {
+
+	var (
+		l   int    // backtrack level
+		i   int    // an index
+		a   *Arc   // a linked list of edges to neighbor
+		u   int    // a vertex
+		vs  []int  // list of vertices
+		is  []int  // list of indices
+		as  []*Arc // list of linked list of edges to neightbors
+		tag []int  // number of times a vertex has been tagged
+	)
+
+	dump := func() {
+		log.Printf("| i=%d, a=%v, v=%d, u=%d", i, a, v, u)
+		log.Printf("| is=%v, as=%v, vs=%v",
+			is[0:l], as[0:l], vs[0:l])
+		log.Printf("| tag=%v", tag)
+	}
+
+	if debug {
+		log.Printf("R1. input graph g")
+		for v := 0; v < g.Order(); v++ {
+			var l strings.Builder
+			l.WriteString(fmt.Sprintf("  v=%d ->", v))
+			for a := Arcs(g, v); a != nil; a = a.next {
+				l.WriteString((fmt.Sprintf(" %d", a.v)))
+			}
+			log.Print(l.String())
+		}
+	}
+
+	// R1. [Initialize.]
+
+	vs = make([]int, n)
+	is = make([]int, n)
+	as = make([]*Arc, n)
+
+	tag = make([]int, g.Order())
+	vs[0] = v
+	i = 0
+	a = Arcs(g, v)
+	as[0] = a
+	tag[v] = 1
+	l = 1
+
+	if debug {
+		log.Printf("R1. Initialized at level %d", l)
+		dump()
+	}
+
+	goto R4
+
+R2:
+	// R2. [Enter level l.]
+
+	if debug {
+		log.Printf("R2. Enter level %d", l)
+		dump()
+	}
+
+	if l == n {
+		visit(vs)
+		l = n - 1
+	}
+
+R3:
+	// R3. [Advance a.]
+	if debug {
+		if a.next != nil {
+			log.Printf("R3. Advance a from %v to %v", a, a.next)
+			dump()
+		}
+	}
+
+	a = a.next
+
+R4:
+	// R4. [Done with level?]
+	if a != nil {
+		goto R5
+	}
+
+	if i == l-1 {
+		goto R6
+	}
+
+	i++
+	v = vs[i]
+	a = Arcs(g, v)
+
+	if debug {
+		log.Printf("R4. Advance i=%d, v=%d, a=%v", i, v, a)
+		dump()
+	}
+
+R5:
+	// R5. [Try a.]
+	u = a.v
+	tag[u]++
+
+	if debug {
+		log.Printf("R5. Try a=%v", a)
+		dump()
+	}
+
+	if tag[u] > 1 {
+		if debug {
+			log.Printf("R5. tag[%d]=%d > 1", u, tag[u])
+		}
+		goto R3
+	}
+
+	is[l] = i
+	as[l] = a
+	vs[l] = u
+	l++
+
+	goto R2
+
+R6:
+	// R6. [Backtrack.]
+	if debug {
+		log.Printf("R6. Backtrack")
+		dump()
+	}
+
+	l--
+	if l == 0 {
+		return
+	}
+
+	i = is[l]
+	v = vs[i]
+
+	// untag all neighbors of v_k, for l >= k > i
+	for k := i + 1; k <= l; k++ {
+		if debug {
+			log.Printf("|  Untagging neighbors of vs[%d]=%d", k, vs[k])
+		}
+		g.Visit(vs[k], func(w int, c int64) bool {
+			tag[w]--
+			return false
+		})
+	}
+
+	a = as[l].next
+	for a != nil {
+		tag[a.v]--
+		a = a.next
+	}
+
+	a = as[l]
+
+	if debug {
+		log.Printf("R6. Untagging complete")
+		dump()
+	}
+
+	goto R3
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -76,3 +76,75 @@ func TestCartesianProduce(t *testing.T) {
 	}
 
 }
+
+func TestArcs(t *testing.T) {
+	cases := []struct {
+		g     *graph.Mutable // graph
+		v     int            // edges from vertex v
+		count int            // number of edges
+	}{
+		{
+			Cycle(5),
+			0,
+			2,
+		},
+		{
+			Complete(5),
+			0,
+			4,
+		},
+	}
+
+	for _, c := range cases {
+		count := 0
+		for a := Arcs(c.g, c.v); a != nil; a = a.next {
+			count++
+		}
+
+		if count != c.count {
+			t.Errorf("Got %d; want %d", count, c.count)
+		}
+	}
+}
+
+func TestConnectedSubsetsVertex(t *testing.T) {
+
+	want := [][]int{
+		{0, 1, 3, 2, 4},
+		{0, 1, 3, 2, 6},
+		{0, 1, 3, 2, 5},
+		{0, 1, 3, 4, 6},
+		{0, 1, 3, 4, 5},
+		{0, 1, 3, 4, 7},
+		{0, 1, 3, 6, 7},
+		{0, 1, 2, 4, 5},
+		{0, 1, 2, 4, 7},
+		{0, 1, 2, 5, 8},
+		{0, 1, 4, 5, 7},
+		{0, 1, 4, 5, 8},
+		{0, 1, 4, 7, 6},
+		{0, 1, 4, 7, 8},
+		{0, 3, 4, 6, 5},
+		{0, 3, 4, 6, 7},
+		{0, 3, 4, 5, 7},
+		{0, 3, 4, 5, 2},
+		{0, 3, 4, 5, 8},
+		{0, 3, 4, 7, 8},
+		{0, 3, 6, 7, 8},
+	}
+
+	var got [][]int
+
+	g := CartesianProduct(Path(3), Path(3))
+
+	ConnectedSubsetsVertex(g, 5, 0, func(solution []int) bool {
+		c := make([]int, 5)
+		copy(c, solution)
+		got = append(got, c)
+		return false
+	})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v; want %v", got, want)
+	}
+}
