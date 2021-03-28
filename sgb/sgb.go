c36f1f93265902f8ff9e963b7a0ff531903d365d
Ben Wallberg <ben.wallberg@gmail.com>
2021-03-28 14:24:21 -0400

    Verify Exercise 7.2.2.1-89, for n < 6

--- sgb/sgb.go
+++ sgb/sgb.go
@@ -15,10 +15,40 @@ func LoadWords() ([]string, error) {
 
 	wordsString, err := box.FindString("sgb-words.txt")
 	if err != nil {
-		return nil, fmt.Errorf("Error reading assets/sgb-words.txt: %s", err)
+		return nil, fmt.Errorf("error reading assets/sgb-words.txt: %s", err)
 	}
 
 	words := strings.Split(wordsString, "\n")
 
 	return words[0 : len(words)-1], nil
+}
+
+// LoadOSPD4 loads the words of The Official Scrabble Players Dictionary,
+// version 4. If size is 0 then load all words, otherwise only load words of
+// length size.
+func LoadOSPD4(size int) ([]string, error) {
+	// Load in ../taocp/assets/ospd4.txt
+	box := packr.NewBox("../taocp/assets")
+
+	wordsString, err := box.FindString("wordlists-ospd4.txt")
+	if err != nil {
+		return nil, fmt.Errorf("error reading assets/wordlists-ospd4.txt: %s", err)
+	}
+
+	words := strings.Split(wordsString, "\n")
+
+	words = words[0 : len(words)-1]
+
+	if size == 0 {
+		return words, nil
+	}
+
+	sizeWords := make([]string, 0)
+	for _, word := range words {
+		if len(word) == size {
+			sizeWords = append(sizeWords, word)
+		}
+	}
+
+	return sizeWords, nil
 }
--- taocp/words_test.go
+++ taocp/words_test.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"reflect"
 	"testing"
+
+	"github.com/wallberg/sandbox/sgb"
 )
 
 func TestDoubleWordSquare(t *testing.T) {
@@ -181,6 +183,104 @@ func TestDoubleWordSquareMinimax(t *testing.T) {
 
 		if !reflect.DeepEqual(got, c.solution) {
 			t.Errorf("Got solution %v for case #%d; want %v", got, i, c.solution)
+		}
+	}
+}
+
+func TestExercise_7221_89(t *testing.T) {
+	// These tests verify Exercise 7.2.2.1-89
+	cases := []struct {
+		size     int
+		solution []string
+	}{
+		{
+			2,
+			[]string{
+				"is", "to",
+				"it", "so",
+			},
+		},
+		{
+			3,
+			[]string{
+				"may", "age", "not",
+				"man", "ago", "yet",
+			},
+		},
+		{
+			4,
+			[]string{
+				"show", "none", "open", "west",
+				"snow", "hope", "ones", "went",
+			},
+		},
+		// { // too slow
+		// 	5,
+		// 	[]string{
+		// 		"start", "three", "roofs", "asset", "peers",
+		// 		"strap", "those", "arose", "refer", "tests",
+		// 	},
+		// },
+		// { // unverified
+		// 	6,
+		// 	[]string{
+		// 		"chests", "lustre", "obtain", "arenas", "circle", "assess",
+		// 		"cloaca", "hubris", "esters", "stance", "trials", "senses",
+		// 	},
+		// },
+		// { // unverified
+		// 	7,
+		// 	[]string{
+		// 		"hertzes", "operate", "mimical", "acerate", "genetic", "endmost", "resents",
+		// 		"homager", "epicene", "remends", "trireme", "zacaton", "etatist", "selects",
+		// 	},
+		// },
+	}
+
+	for i, c := range cases {
+		words, err := sgb.LoadOSPD4(c.size)
+		if err != nil {
+			t.Errorf("Error getting words: %v", err)
+			return
+		}
+
+		stats := &ExactCoverStats{
+			// Progress: true,
+			// Delta:    1000000, //5000000,
+			// Debug:        false,
+			// Verbosity:    2,
+			// SuppressDump: true,
+		}
+
+		xccOptions := &XCCOptions{
+			Minimax:       true,
+			MinimaxSingle: true,
+			Exercise83:    true,
+		}
+
+		var got []string
+		DoubleWordSquare(words, stats, xccOptions, func(s []string) bool {
+			got = s
+			if stats.Debug || stats.Progress {
+				// Determine max word position
+				m := 0
+				for _, word1 := range s {
+					for j, word2 := range words {
+						if word1 == word2 {
+							if j > m {
+								m = j
+							}
+							break
+						}
+					}
+				}
+				log.Printf("m=%d, %v", m, s)
+			}
+			return true
+		})
+
+		if !reflect.DeepEqual(got, c.solution) {
+			t.Errorf("For case #%d got solution %v; want %v", i, got, c.solution)
 		}
 	}
 }
