wallberg/sandbox-go

commit 91df7479f3750cfa4699be8a2386fbbf58c88959

Add benchmarks for SatAlgorithmD()

taocp/sat_algorithm_d.go
@@ -328,21 +328,6 @@ D2:
 
 	k = tail
 
-	if stats != nil {
-		stats.Levels[d]++
-		stats.Nodes++
-
-		if progress {
-			if d > stats.MaxLevel {
-				stats.MaxLevel = d
-			}
-			if stats.Nodes >= stats.Theta {
-				showProgress()
-				stats.Theta += stats.Delta
-			}
-		}
-	}
-
 	//
 	// D3. [Look for unit clauses.]
 	//
@@ -400,6 +385,21 @@ D5:
 	d += 1
 	h[d] = head
 	k = head
+
+	if stats != nil {
+		stats.Levels[d]++
+		stats.Nodes++
+
+		if progress {
+			if d > stats.MaxLevel {
+				stats.MaxLevel = d
+			}
+			if stats.Nodes >= stats.Theta {
+				showProgress()
+				stats.Theta += stats.Delta
+			}
+		}
+	}
 
 	if tail == k {
 		tail = 0

taocp/sat_algorithm_d_test.go
@@ -126,3 +126,69 @@ func TestSatAlgorithmDLangford(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkSatAlgorithmDFromFile(b *testing.B) {
+
+	cases := []struct {
+		filename     string // file name of the SAT data file
+		numVariables int    // number of variable
+		numClauses   int    // number of clauses to satisfy
+		sat          bool   // is satisfiable
+	}{
+		{"testdata/SATExamples/L1.sat", 130, 2437, false},
+		{"testdata/SATExamples/L2.sat", 273, 1020, false},
+		{"testdata/SATExamples/L5.sat", 1472, 102922, true},
+		{"testdata/SATExamples/X2.sat", 129, 354, false},
+		{"testdata/SATExamples/P3.sat", 144, 529, true},
+		{"testdata/SATExamples/P4.sat", 400, 2509, true},
+	}
+
+	for _, c := range cases {
+
+		firstExecution := true
+
+		clauses, variables, _ := SatRead(c.filename)
+
+		b.Run(c.filename, func(b *testing.B) {
+
+			for i := 0; i < b.N; i++ {
+				stats := SatStats{}
+				options := SatOptions{}
+
+				sat, _ := SatAlgorithmD(len(variables), clauses, &stats, &options)
+
+				if firstExecution {
+					b.Logf("SAT=%t, n=%d, m=%d, nodes=%d", sat, len(variables), len(clauses), stats.Nodes)
+					firstExecution = false
+				}
+			}
+
+		})
+	}
+}
+
+func BenchmarkSatAlgorithmDLangford(b *testing.B) {
+
+	for _, n := range []int{5, 9, 13} {
+
+		firstExecution := true
+
+		clauses, coverOptions := SatLangford(n)
+
+		b.Run(fmt.Sprintf("%d", n), func(b *testing.B) {
+
+			for i := 0; i < b.N; i++ {
+				stats := SatStats{}
+				options := SatOptions{}
+
+				sat, _ := SatAlgorithmD(len(coverOptions), clauses, &stats, &options)
+
+				if firstExecution {
+					b.Logf("SAT=%t, n=%d, m=%d, nodes=%d", sat, len(coverOptions), len(clauses), stats.Nodes)
+					firstExecution = false
+				}
+			}
+
+		})
+	}
+}

commit 4ad7dd28cec2b82fd5c19069114452a6bff52d52

Fix bug in SatAlgorithmD()
Introduced by 91df747

taocp/sat_algorithm_d.go
@@ -387,7 +387,7 @@ D5:
 	k = head
 
 	if stats != nil {
-		stats.Levels[d]++
+		stats.Levels[d-1]++
 		stats.Nodes++
 
 		if progress {