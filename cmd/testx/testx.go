7c5fe2ff22bc96a19a0d2438181ba35e6d021f95
Ben Wallberg <wallberg@umd.edu>
2023-03-26 12:47:09 -0400

    Algorithm X in progress
    Pre-testing X4

--- cmd/testx/testx.go
+++ cmd/testx/testx.go
@@ -11,8 +11,8 @@ type data struct {
 	// nodes - all nodes we've seen, ordered by their index value
 	nodes []node
 
-	// S - current node stack
-	S []int
+	// stack - current node stack
+	stack []int
 
 	// indexes - map a vertex v to it's index value
 	indexes map[int]int
@@ -31,10 +31,10 @@ type node struct {
 
 // strongConnect runs Tarjan's algorithm recursivley and outputs a grouping of
 // strongly connected vertices.
-// Returns the v node currently processed, or nil if the visit function requests
-// a halt.
+// Returns: a) *node - the v node currently processed, and b) bool - did we find a contradiction?
+//
 // TODO: add license and acknowledgement
-func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]int) bool) *node {
+func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]int) bool) (*node, bool) {
 
 	// Set the depth index for v to the smallest unused index
 	vIndex := len(data.nodes)
@@ -45,9 +45,9 @@ func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]i
 	data.nodes = append(data.nodes, *vNode)
 
 	// Push v into the stack
-	data.S = append(data.S, v)
+	data.stack = append(data.stack, v)
 
-	// fmt.Printf("v=%d, nodes=%v, S=%v\n", v, data.nodes, data.S)
+	fmt.Printf("v=%d, S=%v\n", v, data.stack)
 
 	// Consider successors of v
 	for i := 0; i < BSIZE[v]; i++ {
@@ -57,10 +57,10 @@ func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]i
 		if !seen {
 
 			// Successor w has not yet been visited; recurse on it
-			wNode := data.strongConnect(w, BIMP, BSIZE, visit)
-			if wNode == nil {
-				// Halt the search
-				return nil
+			wNode, contradiction := data.strongConnect(w, BIMP, BSIZE, visit)
+			if contradiction {
+				// Contradiction found, halt the search
+				return vNode, true
 			}
 
 			// vNode.lowlink = min(vNode.lowlink, wNode.lowlink)
@@ -85,9 +85,9 @@ func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]i
 	// If v is a root node, pop the stack and generate an SCC
 	if vNode.lowlink == vIndex {
 		var scc []int
-		i := len(data.S) - 1
+		i := len(data.stack) - 1
 		for {
-			w := data.S[i]
+			w := data.stack[i]
 			wIndex := data.indexes[w]
 			data.nodes[wIndex].onStack = false
 			scc = append(scc, w)
@@ -96,15 +96,15 @@ func (data *data) strongConnect(v int, BIMP [][]int, BSIZE []int, visit func([]i
 			}
 			i--
 		}
-		data.S = data.S[:i]
+		data.stack = data.stack[:i]
 		fmt.Printf("scc=%v\n", scc)
 		if visit(scc) {
-			// Halt the search
-			return nil
+			// Contradiction found, halt the search
+			return vNode, true
 		}
 	}
 
-	return vNode
+	return vNode, false
 }
 
 func main() {
@@ -119,6 +119,12 @@ func main() {
 
 		// BSIZE - number of clauses for each l in BIMP (literal indexed)
 		BSIZE []int
+
+		// S - number of SCCs found in the dependency graph
+		S int
+
+		// PARENT - parent node in the dependency subforest (literal indexed)
+		PARENT []int
 	)
 
 	test := func() {
@@ -150,7 +156,8 @@ func main() {
 		// candidate literals, have no cycles, and have no nodes with > 1
 		// outbound edge.
 
-		var reps []int
+		// sccs - list of all SCCs
+		var sccs [][]int
 
 		data := &data{
 			nodes:   make([]node, 0, len(vertices)),
@@ -159,44 +166,139 @@ func main() {
 
 		for v := range vertices {
 			if _, seen := data.indexes[v]; !seen {
-				result := data.strongConnect(v, BIMP, BSIZE, func(scc []int) bool {
+				_, contradiction := data.strongConnect(v, BIMP, BSIZE, func(scc []int) bool {
 
-					var rep int // representative from each SCC
-					if len(scc) == 1 {
-						rep = scc[0]
-					} else {
-						// Check if this SCC contains both l and ¬l, and if so
-						// terminate with a contradiction.
+					if len(scc) > 1 {
+
+						// Check if this SCC contains both l and ¬l, and if so terminate with a contradiction.
 						// TODO: Determine if this would be faster with a map
 						for i := 0; i < len(scc); i++ {
 							for j := i + 1; j < len(scc); j++ {
 								if scc[i] == scc[j]^1 {
-									// Contradiction
+									// Contradiction found, halt the search
 									return true
 								}
 							}
 						}
-
-						// TODO: Choose a representatve l with maximum h(l)
-						rep = scc[0]
 					}
 
-					reps = append(reps, rep)
+					sccs = append(sccs, scc)
+
 					return false
 				})
-				if result == nil {
+				if contradiction {
 					fmt.Println("Contradiction!")
 					return
 				}
 			}
 		}
-		fmt.Printf("Representatives=%v", reps)
+
+		// Select a representative from each SCC
+		S = len(sccs)
+		reps := make([]int, S)
+
+		// Select representatives from each SCC of size = 1
+		for r, scc := range sccs {
+
+			if len(scc) == 1 {
+				l := scc[0]
+
+				if BSIZE[l] == 0 {
+					PARENT[l] = 0
+				} else {
+					PARENT[l] = BIMP[l][0] // must select one parent for the subforest
+				}
+				reps[r] = l
+			}
+		}
+
+		// Select representatives from each SCC of size > 1
+		for r, scc := range sccs {
+
+			if len(scc) > 1 {
+
+				// Choose a representatve l with maximum h(l), ensuring that if l is a
+				// representative then ¬l is also a representative
+
+				var l int    // l with maximum h(l) value
+				var maxi int // index of l in reps
+				var maxh int // maximum  value of in reps
+
+				// Search for l ∈ SCC which has maximum h(l) and ¬l is a representative
+				i := 0
+				for ; i < len(scc); i++ {
+
+					// Search for next value of l ∈ SCC which has maximum h(l)
+					maxi = i
+					maxh = scc[i] // TODO: replace simulated h(l) with real value
+
+					for j := i + 1; j < len(scc); j++ {
+						if scc[j] > maxh {
+							maxi = j
+							maxh = scc[j]
+						}
+					}
+
+					l = scc[maxi]
+
+					// Determine if ¬l is a representative
+					isRep := false
+					for _, rep := range reps {
+						if l == rep^1 {
+							isRep = true
+							break
+						}
+					}
+
+					if isRep {
+						// Found the representative we are looking for
+						break
+					} else {
+						// ¬l is not a representative so swap l to the beginning of the list and try again with the
+						// remaining members of the SCC.
+						if maxi != i {
+							scc[i], scc[maxi] = scc[maxi], scc[i]
+						}
+					}
+				}
+
+				if i < len(scc) {
+					// Found a representative l
+					reps[r] = l
+
+					// Set the PARENT for all l ∈ SCC
+					for _, lp := range scc {
+						if l == lp {
+							if BSIZE[l] == 0 {
+								PARENT[l] = 0
+							} else {
+								PARENT[l] = BIMP[l][0] // must select one parent for the subforest
+							}
+						} else {
+							PARENT[lp] = l
+						}
+					}
+				} else {
+					log.Panicf("assertion failed: we did not find any members of l ∈ SCC=%v with a matching ¬l among the other representatives", scc)
+				}
+			}
+		}
+
+		fmt.Printf("S=%d, representatives=%v\n", S, reps)
+		fmt.Print("PARENT: ")
+		for l := 2; l <= 2*n+1; l++ {
+			if l > 2 {
+				fmt.Print(", ")
+			}
+			fmt.Printf("%d=%d", l, PARENT[l])
+		}
 		fmt.Println()
 	}
 
 	n = 4
 	BIMP = make([][]int, 2*n+2)
 	BSIZE = make([]int, 2*n+2)
+	PARENT = make([]int, 2*n+2)
 	for l := 2; l <= 2*n+1; l++ {
 		switch l {
 		case 3:
@@ -220,6 +322,7 @@ func main() {
 	n = 9
 	BIMP = make([][]int, 2*n+2)
 	BSIZE = make([]int, 2*n+2)
+	PARENT = make([]int, 2*n+2)
 	for l := 2; l <= 2*n+1; l++ {
 		switch l {
 		case 3:
@@ -249,8 +352,11 @@ func main() {
 	n = 4
 	BIMP = make([][]int, 2*n+2)
 	BSIZE = make([]int, 2*n+2)
+	PARENT = make([]int, 2*n+2)
 	for l := 2; l <= 2*n+1; l++ {
 		switch l {
+		case 2:
+			BIMP[l] = []int{8}
 		case 3:
 			BIMP[l] = []int{5, 7}
 		case 4:
@@ -262,7 +368,7 @@ func main() {
 		case 7:
 			BIMP[l] = []int{9}
 		case 8:
-			BIMP[l] = []int{6}
+			BIMP[l] = []int{4, 6}
 		case 9:
 			BIMP[l] = []int{3}
 		default:
